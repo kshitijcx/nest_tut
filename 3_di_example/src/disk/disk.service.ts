c8d81a9ac96d9128379a342dc10e2cff9cf16fce
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-02-02 18:23:02 +0530

    connect services and modules, add methods to services

--- 3_di_example/src/disk/disk.service.ts
+++ 3_di_example/src/disk/disk.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+  getData() {
+    console.log('Drawing 5W of power from Power Service');
+    this.powerService.supplyPower(5);
+    return 'Confidential Data';
+  }
+}
--- 3_di_example/src/power/power.service.ts
+++ 3_di_example/src/power/power.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PowerService {
-  supplePower(watts: number) {
+  supplyPower(watts: number) {
     console.log(`Supplying ${watts}W of power`);
   }
 }
