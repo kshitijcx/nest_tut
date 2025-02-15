db4a535ff565f3c0436a8e9d9775234220e74d7a
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-02-15 22:02:02 +0530

    add find, findOne, update, remove services

--- 4_car_value_api/src/users/user.entity.ts
+++ 4_car_value_api/src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -7,7 +14,22 @@ export class User {
 
   @Column()
   email: string;
-  
+
   @Column()
   password: string;
+
+  @AfterInsert() //hooks -> do some action when something happens
+  logger() {
+    console.log('Inserted User with ID', this.id);
+  }
+
+  @AfterRemove()
+  logUpdate() {
+    console.log('Updated User with ID', this.id);
+  }
+
+  @AfterUpdate()
+  logRemove() {
+    console.log('Removed User with ID', this.id);
+  }
 }
