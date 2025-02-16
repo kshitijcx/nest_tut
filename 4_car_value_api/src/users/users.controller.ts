a7e199ca0fb4d8b97b6bf54c182a3824696b64ad
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-02-16 17:12:16 +0530

    add NotFoundException, exclude passwords from reponse

--- 4_car_value_api/src/users/users.controller.ts
+++ 4_car_value_api/src/users/users.controller.ts
@@ -7,6 +7,9 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,9 +24,14 @@ export class UsersController {
     this.usersService.createUser(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   @Get()
