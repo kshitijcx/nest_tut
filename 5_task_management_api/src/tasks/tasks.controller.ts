3073f3052d2290b418194e09c6500aaa27f9c0a2
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-02-22 23:43:49 +0530

    add methods to modify in memory tasks array

--- 5_task_management_api/src/tasks/tasks.controller.ts
+++ 5_task_management_api/src/tasks/tasks.controller.ts
@@ -1,12 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   //   tasksService: TasksService;
   constructor(private tasksService: TasksService) {}
 
-  helloWorld() {
-    
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('desciption') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
