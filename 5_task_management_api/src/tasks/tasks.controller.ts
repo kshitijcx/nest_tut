c49271e56baaf6679d70c364d4e24c14c6aca87a
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-02-23 13:57:42 +0530

    add all crud methods for in memory tasks array

--- 5_task_management_api/src/tasks/tasks.controller.ts
+++ 5_task_management_api/src/tasks/tasks.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { GetTaskFilterDto } from './dtos/get-task-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -8,15 +19,38 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilter(filterDto)
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
   createTask(
-    @Body('title') title: string,
-    @Body('desciption') description: string,
+    // @Body('title') title: string,
+    // @Body('desciption') description: string,
+    @Body() createTaskDto: CreateTaskDto,
   ): Task {
-    return this.tasksService.createTask(title, description);
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ) {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
